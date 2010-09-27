a64c3ef0681caf2deaaa0d07bfa249ceb10f028f
guiott@gmail.com <guiott@gmail.com@70efd0fa-daa1-12ff-fe29-736cb8c56a6e>
2010-09-27 22:11:16 +0000

    still development

--- trunk/ExplorerSound/definitions.h
+++ trunk/ExplorerSound/definitions.h
@@ -7,7 +7,7 @@ BYTE str[7];		// for TX procedures
 
 // Automatic Gain Control 
 // 0=1/16, 1=1, 2=2, 3=4; 4=8, 5=16, 6=24, 7=48
-BYTE GF[][]={0, 0, 0xF8, 1, 0x78, 2, 0x38, 4, 
+BYTE GF[8][2]={0, 0, 0xF8, 1, 0x78, 2, 0x38, 4, 
 			0x18, 8, 0x08, 16, 0x1C, 24, 0x0C, 48};
 // current index of Gain Factor (GF) for each input
 // 0 = PGA Pre, 1 = PGA Out
